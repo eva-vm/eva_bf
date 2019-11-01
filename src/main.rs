071513cfc98a0d42ec198af33dc8f8b9e86a8ac3
Arthur Correnson <arthur.correnson@gmail.com>
2019-11-01 16:00:29 +0100

    get the compiler to work

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ mod parse;
 mod code_generation;
 
 fn main() {
+    // let parsing_result = parse::parse("++[-]");
     let parsing_result = parse::parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
     match parsing_result {
         Ok(p) => {
             eprintln!("{:?}", p);
             match code_generation::generate(&p, &mut std::io::stdout()) {
-              Ok(()) => eprintln!("✅ Done."),
+              Ok(_) => eprintln!("✅ Done."),
               Err(_) => eprintln!("❌ Ouput Error while generating the code."),
             }
         },
--- src/parse.rs
+++ src/parse.rs
@@ -1,27 +1,28 @@
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Command {
-    Inc,
-    Dec,
-    Shift,
-    Unshift,
+    Inc(usize),
+    Dec(usize),
+    Shift(usize),
+    Unshift(usize),
     Input,
     Output,
 }
-// COUCOU!
+
 #[derive(Clone, Debug)]
 pub enum Program {
     Command(Command),
     Sequence(Box<Vec<Program>>),
+    Program(Box<Vec<Program>>)
 }
 
 peg::parser! {
     grammar brainfuck() for str {
         use super::{Command, Program};
         rule ignore() = quiet!{[' ' | '\t' | '\n']}
-        rule inc()      -> Command = "+" {Command::Inc}
-        rule dec()      -> Command = "-" {Command::Dec}
-        rule shift()    -> Command = ">" {Command::Shift}
-        rule unshift()  -> Command = "<" {Command::Unshift}
+        rule inc()      -> Command = v:$("+"+) {Command::Inc(v.len())}
+        rule dec()      -> Command = v:$("-"+) {Command::Dec(v.len())}
+        rule shift()    -> Command = v:$(">"+) {Command::Shift(v.len())}
+        rule unshift()  -> Command = v:$("<"+) {Command::Unshift(v.len())}
         rule input()    -> Command = "," {Command::Input}
         rule output()   -> Command = "." {Command::Output}
 
@@ -32,7 +33,7 @@ peg::parser! {
             Program::Sequence(Box::from(l))
         }
 
-        pub rule program() -> Program = p:(command() / lop())* { Program::Sequence(Box::from(p)) }
+        pub rule program() -> Program = p:(command() / lop())* { Program::Program(Box::from(p)) }
     }
 }
 
