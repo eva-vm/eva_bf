e922b8f66d30aa4e50e4eb8176e979c57b74b726
Nathan Graule <solarliner@gmail.com>
2019-11-01 21:37:55 +0100

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,71 @@
+use std::io::Read;
+
+use clap::{App, Arg};
+
+use crate::utils::{Input, Output};
+
+mod generate;
 mod parse;
-mod code_generation;
+mod utils;
+
+pub const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 fn main() {
-    // let parsing_result = parse::parse("++[-]");
-    let parsing_result = parse::parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-    match parsing_result {
-        Ok(p) => {
-            eprintln!("{:?}", p);
-            match code_generation::generate(&p, &mut std::io::stdout()) {
-              Ok(_) => eprintln!("✅ Done."),
-              Err(_) => eprintln!("❌ Ouput Error while generating the code."),
-            }
-        },
-        Err(err) => eprintln!("Parsing error at {}", err),
-    }
+	// let parsing_result = parse::parse("++[-]");
+	/*let parsing_result = parse::parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
+	match parsing_result {
+		Ok(p) => {
+			eprintln!("{:?}", p);
+			match code_generation::generate(&p, &mut std::io::stdout()) {
+			  Ok(_) => eprintln!("✅ Done."),
+			  Err(_) => eprintln!("❌ Ouput Error while generating the code."),
+			}
+		},
+		Err(err) => eprintln!("Parsing error at {}", err),
+	}*/
+	let matches = App::new("Eva Brainfuck compiler")
+		.version(VERSION.unwrap_or("unstable"))
+		.author("Nathan G. <solarliner@gmail.com>, Arthur C. <arthur.correnson@gmail.com>")
+		.about("Compiles Brainfuck to Eva ASM")
+		.arg(
+			Arg::with_name("OUTPUT")
+				.short("o")
+				.long("output")
+				.help("Output compiled/asm program"),
+		)
+		.arg(
+			Arg::with_name("INPUT")
+				.index(1)
+				.help("Input brainfuck program"),
+		)
+		.arg(
+			Arg::with_name("v")
+				.short("v")
+				.multiple(true)
+				.help("Sets the level of verbosity"),
+		)
+		.get_matches();
+	let input_file = Input::from_arg(matches.value_of("INPUT"));
+	let output_file = Output::from_arg(matches.value_of("OUTPUT"));
+
+	match (input_file, output_file) {
+		(Err(e), _) | (_, Err(e)) => eprintln!("{}", e),
+		(Ok(mut input), Ok(mut output)) => {
+			let mut inbuf = String::new();
+			let res = input
+				.read_to_string(&mut inbuf)
+				.map_err(|err| format!("Couldn't read input: {}", err))
+				.and_then(|_| {
+					parse::parse(&inbuf)
+						.map_err(|err| format!("Couldn't parse input: {}", err))
+						.and_then(|ast| {
+							generate::generate(&ast, &mut output)
+								.map_err(|err| format!("Couldn't generate assembly: {}", err))
+						})
+				});
+			if let Err(e) = res {
+				eprintln!("ERROR: {}", e);
+			}
+		}
+	}
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,42 +1,42 @@
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Command {
-    Inc(usize),
-    Dec(usize),
-    Shift(usize),
-    Unshift(usize),
-    Input,
-    Output,
+	Inc(usize),
+	Dec(usize),
+	Shift(usize),
+	Unshift(usize),
+	Input,
+	Output,
 }
 
 #[derive(Clone, Debug)]
 pub enum Program {
-    Command(Command),
-    Sequence(Box<Vec<Program>>),
-    Program(Box<Vec<Program>>)
+	Command(Command),
+	Sequence(Box<Vec<Program>>),
+	Program(Box<Vec<Program>>),
 }
 
 peg::parser! {
-    grammar brainfuck() for str {
-        use super::{Command, Program};
-        rule ignore() = quiet!{[' ' | '\t' | '\n']}
-        rule inc()      -> Command = v:$("+"+) {Command::Inc(v.len())}
-        rule dec()      -> Command = v:$("-"+) {Command::Dec(v.len())}
-        rule shift()    -> Command = v:$(">"+) {Command::Shift(v.len())}
-        rule unshift()  -> Command = v:$("<"+) {Command::Unshift(v.len())}
-        rule input()    -> Command = "," {Command::Input}
-        rule output()   -> Command = "." {Command::Output}
+	grammar brainfuck() for str {
+		use super::{Command, Program};
+		rule ignore() = quiet!{[' ' | '\t' | '\n']+}
+		rule inc()      -> Command = v:$("+"+) {Command::Inc(v.len())}
+		rule dec()      -> Command = v:$("-"+) {Command::Dec(v.len())}
+		rule shift()    -> Command = v:$(">"+) {Command::Shift(v.len())}
+		rule unshift()  -> Command = v:$("<"+) {Command::Unshift(v.len())}
+		rule input()    -> Command = "," {Command::Input}
+		rule output()   -> Command = "." {Command::Output}
 
-        rule command()  -> Program = c:(inc() / dec() / shift() / unshift() / input() / output()) {
-            Program::Command(c)
-        }
-        rule lop()      -> Program = "[" l:(command() / lop())+ "]" {
-            Program::Sequence(Box::from(l))
-        }
+		rule command()  -> Program = c:(inc() / dec() / shift() / unshift() / input() / output()) {
+			Program::Command(c)
+		}
+		rule lop()      -> Program = "[" l:(command() / lop())+ "]" {
+			Program::Sequence(Box::from(l))
+		}
 
-        pub rule program() -> Program = p:(command() / lop())* { Program::Program(Box::from(p)) }
-    }
+		pub rule program() -> Program = p:(command() / lop())* { Program::Program(Box::from(p)) }
+	}
 }
 
 pub fn parse(input: &str) -> Result<Program, peg::error::ParseError<peg::str::LineCol>> {
-    brainfuck::program(input)
+	brainfuck::program(input)
 }
