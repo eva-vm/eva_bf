0b0e25a501eec6e5da598d4c8046835a8669179f
Nathan Graule <solarliner@gmail.com>
2019-11-04 14:11:11 +0100

    Implement assembly CLI option

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,14 @@
-use std::io::Read;
+use std::io::{Cursor, Read, Write};
 
 use clap::{App, Arg};
-
-use crate::utils::{Input, Output};
+use wbuf::{Input, Output};
 
 mod generate;
 mod parse;
-mod utils;
 
 pub const VERSION: Option<&'static str> = option_env!("CARGO_PKG_VERSION");
 
 fn main() {
-	// let parsing_result = parse::parse("++[-]");
-	/*let parsing_result = parse::parse("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-	match parsing_result {
-		Ok(p) => {
-			eprintln!("{:?}", p);
-			match code_generation::generate(&p, &mut std::io::stdout()) {
-			  Ok(_) => eprintln!("✅ Done."),
-			  Err(_) => eprintln!("❌ Ouput Error while generating the code."),
-			}
-		},
-		Err(err) => eprintln!("Parsing error at {}", err),
-	}*/
 	let matches = App::new("Eva Brainfuck compiler")
 		.version(VERSION.unwrap_or("unstable"))
 		.author("Nathan G. <solarliner@gmail.com>, Arthur C. <arthur.correnson@gmail.com>")
@@ -31,6 +17,7 @@ fn main() {
 			Arg::with_name("OUTPUT")
 				.short("o")
 				.long("output")
+				.takes_value(true)
 				.help("Output compiled/asm program"),
 		)
 		.arg(
@@ -44,27 +31,63 @@ fn main() {
 				.multiple(true)
 				.help("Sets the level of verbosity"),
 		)
+		.arg(Arg::with_name("asm").short("a").long("assemble"))
 		.get_matches();
-	let input_file = Input::from_arg(matches.value_of("INPUT"));
-	let output_file = Output::from_arg(matches.value_of("OUTPUT"));
+	if matches.is_present("asm") {
+		let input_file = Input::from_arg(matches.value_of("INPUT"));
+		let output_file = Output::from_arg(matches.value_of("OUTPUT"));
 
-	match (input_file, output_file) {
-		(Err(e), _) | (_, Err(e)) => eprintln!("{}", e),
-		(Ok(mut input), Ok(mut output)) => {
-			let mut inbuf = String::new();
-			let res = input
-				.read_to_string(&mut inbuf)
-				.map_err(|err| format!("Couldn't read input: {}", err))
-				.and_then(|_| {
-					parse::parse(&inbuf)
-						.map_err(|err| format!("Couldn't parse input: {}", err))
-						.and_then(|ast| {
-							generate::generate(&ast, &mut output)
-								.map_err(|err| format!("Couldn't generate assembly: {}", err))
-						})
-				});
-			if let Err(e) = res {
-				eprintln!("ERROR: {}", e);
+		match (input_file, output_file) {
+			(Err(e), _) | (_, Err(e)) => eprintln!("{}", e),
+			(Ok(mut input), Ok(mut output)) => {
+				let mut inbuf = String::new();
+				let res = input
+					.read_to_string(&mut inbuf)
+					.map_err(|err| format!("Couldn't read input: {}", err))
+					.and_then(|_| {
+						parse::parse(&inbuf)
+							.map_err(|err| format!("Couldn't parse input: {}", err))
+							.and_then(|ast| {
+								generate::generate(&ast, &mut output)
+									.map_err(|err| format!("Couldn't generate assembly: {}", err))
+							})
+					});
+				match res {
+					Ok(size) => eprintln!("✔ Done ({} bytes).", size),
+					Err(e) => eprintln!("❌ Assembly error: {}", e),
+				}
+			}
+		}
+	} else {
+		let input_file = Input::from_arg(matches.value_of("INPUT"));
+		let mut temp_asm: Cursor<Vec<u8>> = Cursor::new(Vec::new());
+		let output_file = Output::from_arg(matches.value_of("OUTPUT"));
+
+		match (input_file, output_file) {
+			(Err(e), _) | (_, Err(e)) => eprintln!("{}", e),
+			(Ok(mut input), Ok(mut output)) => {
+				let mut input_buffer = String::new();
+				let res = input
+					.read_to_string(&mut input_buffer)
+					.map_err(|err| format!("Couldn't read input: {}", err))
+					.and_then(|_| {
+						parse::parse(&input_buffer)
+							.map_err(|err| format!("Couldn't parse input: {}", err))
+					})
+					.and_then(|ast| {
+						generate::generate(&ast, &mut temp_asm)
+							.map_err(|err| format!("Couldn't generate assembly: {}", err))
+					})
+					.and_then(|_| {
+						temp_asm
+							.read_to_string(&mut input_buffer)
+							.map_err(|e| format!("ASM read error: {}", e))
+					})
+					.and_then(|_| evasm::assemble(&input_buffer, &mut output));
+				match res {
+					Err(e) => eprintln!("❌ Generation error: {}", e),
+					Ok(size) => eprintln!("✔ Done ({} bytes).", size),
+				}
 			}
 		}
 	}
